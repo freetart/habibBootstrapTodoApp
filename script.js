47646dab1702c04b9f94d92342a4eb89c65eca3b
Chris M <habibdev96@gmail.com>
2021-02-15 11:53:37 -0700

    selected dom elements

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 "use strict";
+
+// selectors
+const form = document.querySelector(".form");
+const todoInput = document.querySelector(".todo-input");
+const submitTodoBtn = document.querySelector(".submit-todo-btn");
+const clearAllBtn = document.querySelector(".clear-all-btn");
+const checkBtn = document.querySelector(".check-btn");
+const deleteBtn = document.querySelector(".delete-btn");
